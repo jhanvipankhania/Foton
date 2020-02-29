1c7cde2d87067c4467b6e2c93cc026f22f6d63a1
Urmi Shah <urmi18shah@gmail.com>
2020-02-29 12:39:10 +0530

    Update AIMG.py
    Added the feature of capturing image dataset via webcam :
    -Press Space to capture the image
    -Press Esc for closing the webcam

--- AIMG.py
+++ AIMG.py
@@ -15,6 +15,8 @@ from PySide2.QtWidgets import *
 from PySide2.QtWidgets import QApplication, QWidget, QLabel
 from PySide2.QtGui import QIcon, QPixmap
 
+from time import sleep
+
 
 
 
@@ -105,7 +107,7 @@ class K_means:
 
 
 class groupImgGUI(QWidget):
-
+#--->MAIN WINDOW#
     def __init__(self, parent=None):
         super(groupImgGUI, self).__init__(parent)
         self.dir = None
@@ -113,91 +115,79 @@ class groupImgGUI(QWidget):
         self.width = 400
         self.height = 300
 
-        self.createSettings()
-        self.editSettings()
-        self.nextFeatures()
+        self.createSettings() #image grouping box
+        self.editSettings() #edit image box
+        #self.datasetSettings() #dataset box
+        self.nextFeatures() #total features list box
         layout = QVBoxLayout()
 
         self.b1 = QPushButton("Start")
         self.b1.setCheckable(True)
         self.b1.toggle()
-        self.b1.clicked.connect(self.state3)
+        self.b1.clicked.connect(self.state3) #redirects to the tool function
 
         self.label = QLabel(self)
-        self.label.setPixmap(QPixmap('image.jpeg'))
+        self.label.setPixmap(QPixmap('image4.png')) #bg image
         self.label.setGeometry(0, 0, 400, 300) #left, top, width, height
 
-        layout.addWidget(self.formGroupBox3)
+        layout.addWidget(self.formGroupBox3) #redirects to another window
 
-        layout.addWidget(self.b1)
+        layout.addWidget(self.b1) #intitialized button b1(start) onto the main window
         self.setMinimumSize(400, 300)
-        self.setLayout(layout)
+        self.setLayout(layout) #the main window
         self.setWindowTitle("Foton")
 
+#--->TOOL/FEATURE FUNCTION#
+    def state3(self):
+        if self.b1.isChecked():
+            self.formGroupBox3.show()
+            self.b1.hide()
+        else:
+            self.formGroupBox3.hide()
 
+#TOOL/FEATURE WINDOW#
     def nextFeatures(self):
-        self.formGroupBox3 = QGroupBox("Foton")
-
-
+        self.formGroupBox3 = QGroupBox("Tools")
         self.btn2 = QPushButton("Image Grouping")
         self.btn2.setCheckable(False)
         self.btn2.toggle()
-        self.btn2.clicked.connect(self.state)
+        self.btn2.clicked.connect(self.state) #redirects to its function 'state'
         self.btn3 = QPushButton("Edit")
         self.btn3.setCheckable(False)
         self.btn3.toggle()
-        self.btn3.clicked.connect(self.state2)
+        self.btn3.clicked.connect(self.state2) #redirects to its function 'state2'
+        self.btn4 = QPushButton("Dataset")
+        self.btn4.setCheckable(False)
+        self.btn4.toggle()
+        self.btn4.clicked.connect(self.state4)  # redirects to its function 'state3'
 
-
-        vbox = QVBoxLayout()
+        vbox = QVBoxLayout() #tools window
         self.formGroupBox3.hide()
         self.formGroupBox3.setLayout(vbox)
 
         vbox.addWidget(self.btn2)
         vbox.addWidget(self.btn3)
+        vbox.addWidget(self.btn4)
         vbox.addStretch(0)
 
 
 
-    def selectFolder(self):
-        QFileDialog.FileMode(QFileDialog.Directory)
-        self.dir = QFileDialog.getExistingDirectory(self)
-        self.btn.setText(self.dir or "Select folder")
 
-
-
+#--->IMAGE GROUPING FUNCTION#
     def state(self):
         if self.btn2.isChecked():
-            self.formGroupBox.hide()
+            self.formGroupBox.hide() #opens another window
         else:
             self.formGroupBox.show()
 
-
-
-
-    def state2(self):
-        if self.btn3.isChecked():
-            self.formGroupBox2.hide()
-        else:
-            self.formGroupBox2.show()
-
-
-    def state3(self):
-        if self.b1.isChecked():
-            self.formGroupBox3.show()
-            self.b1.hide()
-
-        else:
-            self.formGroupBox3.hide()
-
-
-    def createSettings(self):
+#IMAGE GROUPING WINDOW#
+    def createSettings(self): #image grouping window
         self.formGroupBox = QGroupBox("Settings")
         flayout = QFormLayout()
         self.formGroupBox.hide()
         self.formGroupBox.setLayout(flayout)
         self.btn = QPushButton("Select folder")
-        self.btn.clicked.connect(self.selectFolder)
+        self.btn.clicked.connect(self.selectFolder) #redirects to select folder function
         self.kmeans = QSpinBox()
         self.kmeans.setRange(2, 15)
         self.kmeans.setValue(2)
@@ -216,116 +206,13 @@ class groupImgGUI(QWidget):
         flayout.addRow(QLabel("Size:"), self.size)
         flayout.addRow(self.runbtn)
 
+#SELECT FOLDER FUNCTION#
+    def selectFolder(self):
+        QFileDialog.FileMode(QFileDialog.Directory)
+        self.dir = QFileDialog.getExistingDirectory(self)
+        self.btn.setText(self.dir or "Select folder")
 
-
-
-
-
-    def editSettings(self):
-
-        self.formGroupBox2 = QGroupBox("Edit")
-        self.img = QPushButton('Open Image')
-        layout2 = QFormLayout()
-        hbox_address = QHBoxLayout()
-        self.address = QLineEdit()
-        hbox_address.addWidget(self.address)
-        self.inp = QLineEdit()
-        self.img.clicked.connect(self.open)
-
-        hbox_size = QHBoxLayout()
-        self.label_width = QLabel('Width :')
-        self.label_height = QLabel('Height :')
-        self.et_width = QLineEdit()
-        self.et_height = QLineEdit()
-        hbox_size.addWidget(self.label_width)
-        hbox_size.addWidget(self.et_width)
-        hbox_size.addWidget(self.label_height)
-        hbox_size.addWidget(self.et_height)
-
-        hbox_colorscale = QHBoxLayout()
-        self.color_scale = QLabel('Color Scale :')
-        self.Grey_scale = QRadioButton('Grey', self)
-        self.Hsv = QRadioButton('HSV ', self)
-        hbox_colorscale.addWidget(self.color_scale)
-        hbox_colorscale.addWidget(self.Grey_scale)
-        hbox_colorscale.addWidget(self.Hsv)
-
-        hbox_save = QHBoxLayout()
-        self.address_save = QLineEdit()
-        hbox_save.addWidget(self.address_save)
-        self.btn_save = QPushButton('Save Image')
-        self.btn_save.clicked.connect(self.save)
-        hbox_save.addWidget(self.btn_save)
-
-        self.img_processed = False
-        self.btn_process_img = QPushButton("Process Image")
-        self.btn_process_img.clicked.connect(self.getInput)
-        hbox_btn = QHBoxLayout()
-        hbox_btn.addWidget(self.btn_process_img)
-
-
-        layout2.addRow(self.img, self.address)
-        layout2.addRow(self.label_height , self.et_height)
-        layout2.addRow(self.label_width, self.et_width)
-        layout2.addRow(self.color_scale)
-        layout2.addRow(self.Grey_scale, self.Hsv)
-        layout2.addRow(self.btn_process_img)
-        layout2.addRow(self.btn_save)
-
-
-
-        self.formGroupBox2.hide()
-        self.formGroupBox2.setLayout(layout2)
-
-        # @pyqtSlot()
-
-    def getInput(self):
-        self.req_height = self.et_height.text()
-        self.req_width = self.et_width.text()
-        if self.req_width != '' and self.req_height != '':
-            self.ready = True
-            self.img_processed = True
-        else:
-            self.ready = False
-
-        if self.ready is False:
-            QMessageBox.about(self, 'Error', 'Fill parameters to process')
-        elif self.address.text() is '':
-            QMessageBox.about(self, 'Error', 'Select Image to process')
-        else:
-            self.req_img = self.process_img(cv2.imread(self.address.text()))
-            cv2.imshow("req_img", self.req_img)
-
-        # print(self.req_height,self.req_width)
-
-    def process_img(self, imgtoproc):
-        if self.Grey_scale.isChecked():
-            imgtoproc = cv2.cvtColor(imgtoproc, cv2.COLOR_BGR2GRAY)
-        elif self.Hsv.isChecked():
-            imgtoproc = cv2.cvtColor(imgtoproc, cv2.COLOR_BGR2HSV)
-
-        return cv2.resize(imgtoproc, (int(self.req_width), int(self.req_height)))
-
-    def open(self):
-        fileName = QFileDialog.getOpenFileName(self, 'openFile')
-        self.address.setText(fileName[0])
-        self.showImage(fileName[0])
-        # print(fileName)
-
-
-    def showImage(self,address):
-        img = cv2.imread(address)
-        cv2.imshow('Yo',img)
-
-    def save(self):
-        if self.img_processed:
-            saveFile = QFileDialog.getSaveFileName(self, 'saveFile')
-            self.address_save.setText(saveFile[0])
-            if saveFile[0] != '':
-                cv2.imwrite(str(self.address_save.text()), self.req_img)
-        else:
-            QMessageBox.about(self, 'Suggestion', 'Do Something')
-
+#PROGRESS BUTTONS FUNCTION#
     def disableButton(self):
         self.runbtn.setText("Working...")
         self.runbtn.setEnabled(False)
@@ -364,6 +251,150 @@ class groupImgGUI(QWidget):
             action(k.end[i], folder + "/" + str(k.cluster[i] + 1).zfill(nfolders) + "/")
         QMessageBox.information(self, "Done", 'Done!')
         self.enableButton()
+
+
+
+#--->EDIT FUNCTION#
+    def state2(self):
+         if self.btn3.isChecked():
+             self.formGroupBox2.hide()
+         else:
+             self.formGroupBox2.show()
+#EDIT WINDOW#
+    def editSettings(self):
+
+        self.formGroupBox2 = QGroupBox("Edit")
+        self.img = QPushButton('Open Image')
+        layout2 = QFormLayout()
+        hbox_address = QHBoxLayout()
+        self.address = QLineEdit()
+        hbox_address.addWidget(self.address)
+        self.inp = QLineEdit()
+        self.img.clicked.connect(self.open)
+
+        hbox_size = QHBoxLayout()
+        self.label_width = QLabel('Width :')
+        self.label_height = QLabel('Height :')
+        self.et_width = QLineEdit()
+        self.et_height = QLineEdit()
+        hbox_size.addWidget(self.label_width)
+        hbox_size.addWidget(self.et_width)
+        hbox_size.addWidget(self.label_height)
+        hbox_size.addWidget(self.et_height)
+
+        hbox_colorscale = QHBoxLayout()
+        self.color_scale = QLabel('Color Scale :')
+        self.Grey_scale = QRadioButton('Grey', self)
+        self.Hsv = QRadioButton('HSV ', self)
+        hbox_colorscale.addWidget(self.color_scale)
+        hbox_colorscale.addWidget(self.Grey_scale)
+        hbox_colorscale.addWidget(self.Hsv)
+
+        hbox_save = QHBoxLayout()
+        self.address_save = QLineEdit()
+        hbox_save.addWidget(self.address_save)
+        self.btn_save = QPushButton('Save Image')
+        self.btn_save.clicked.connect(self.save)
+        hbox_save.addWidget(self.btn_save)
+
+        self.img_processed = False
+        self.btn_process_img = QPushButton("Process Image")
+        self.btn_process_img.clicked.connect(self.getInput) #redirects to getinput function
+        hbox_btn = QHBoxLayout()
+        hbox_btn.addWidget(self.btn_process_img)
+
+        layout2.addRow(self.img, self.address)
+        layout2.addRow(self.label_height , self.et_height)
+        layout2.addRow(self.label_width, self.et_width)
+        layout2.addRow(self.color_scale)
+        layout2.addRow(self.Grey_scale, self.Hsv)
+        layout2.addRow(self.btn_process_img)
+        layout2.addRow(self.btn_save)
+
+        self.formGroupBox2.hide()
+        self.formGroupBox2.setLayout(layout2)
+
+#GETINPUT FUNCTION#
+    def getInput(self):
+        self.req_height = self.et_height.text()
+        self.req_width = self.et_width.text()
+        if self.req_width != '' and self.req_height != '':
+            self.ready = True
+            self.img_processed = True
+        else:
+            self.ready = False
+
+        if self.ready is False:
+            QMessageBox.about(self, 'Error', 'Fill parameters to process')
+        elif self.address.text() is '':
+            QMessageBox.about(self, 'Error', 'Select Image to process')
+        else:
+            self.req_img = self.process_img(cv2.imread(self.address.text()))
+            cv2.imshow("req_img", self.req_img)
+        # print(self.req_height,self.req_width)
+
+#IMAGE PROCESS FUNCTION#
+    def process_img(self, imgtoproc):
+        if self.Grey_scale.isChecked():
+            imgtoproc = cv2.cvtColor(imgtoproc, cv2.COLOR_BGR2GRAY)
+        elif self.Hsv.isChecked():
+            imgtoproc = cv2.cvtColor(imgtoproc, cv2.COLOR_BGR2HSV)
+
+        return cv2.resize(imgtoproc, (int(self.req_width), int(self.req_height)))
+
+#OPEN IMAGE FUNCTION#
+    def open(self):
+        fileName = QFileDialog.getOpenFileName(self, 'openFile')
+        self.address.setText(fileName[0])
+        self.showImage(fileName[0])
+        # print(fileName)
+
+#SHOWIMAGE FUNCTION#
+    def showImage(self,address):
+        img = cv2.imread(address)
+        cv2.imshow('Yo',img)
+
+#SAVE IMAGE FUNCTION#
+    def save(self):
+        if self.img_processed:
+            saveFile = QFileDialog.getSaveFileName(self, 'saveFile')
+            self.address_save.setText(saveFile[0])
+            if saveFile[0] != '':
+                cv2.imwrite(str(self.address_save.text()), self.req_img)
+        else:
+            QMessageBox.about(self, 'Suggestion', 'Do Something')
+
+
+#--->DATASET FUNCTION#
+    def state4(self):
+            cam = cv2.VideoCapture(0)
+            cv2.namedWindow("test")
+            img_counter = 0
+
+            while True:
+                ret, frame = cam.read()
+                cv2.imshow("test", frame)
+                if not ret:
+                    break
+                k = cv2.waitKey(1)
+
+                if k % 256 == 27:
+                    # ESC pressed
+                    print("Escape hit, closing...")
+                    break
+                elif k % 256 == 32:
+                    # SPACE pressed
+                    img_name = "opencv_frame_{}.png".format(img_counter)
+                    cv2.imwrite(img_name, frame)
+                    print("{} written!".format(img_name))
+                    img_counter += 1
+
+            cam.release()
+            cv2.destroyAllWindows()
+
+
+
+
 
 
 def main():
